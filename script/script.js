f9499f9c6ac4f89464ff28a4c6c40d2b2f798e00
Benzazoua <billalben252@gmail.com>
2024-04-21 13:56:34 +0100

    Time countdown

---
+++ script/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/**
+ * time countdown
+ */
+
+const elementsTime = {
+  seconds: document.querySelector(".time .unit .seconds"),
+  minutes: document.querySelector(".time .unit .minutes"),
+  hours: document.querySelector(".time .unit .hours"),
+  days: document.querySelector(".time .unit .days"),
+};
+
+let values = {
+  seconds: 11,
+  minutes: 2,
+  hours: 2,
+  days: 9,
+};
+
+function updateValue(key, value) {
+  elementsTime[key].textContent = String(value).padStart(2, "0");
+}
+
+const timeFunction = setInterval(() => {
+  values.seconds--;
+
+  if (values.seconds === 0) {
+    values.minutes--;
+    values.seconds = 59;
+  }
+  if (values.minutes === 0) {
+    values.hours--;
+    values.minutes = 59;
+  }
+  if (values.hours === 0) {
+    values.days--;
+    values.hours = 23;
+  }
+
+  if (values.days === 0) clearInterval(timeFunction);
+
+  updateValue("seconds", values.seconds);
+  updateValue("minutes", values.minutes);
+  updateValue("hours", values.hours);
+  updateValue("days", values.days);
+}, 1000);
